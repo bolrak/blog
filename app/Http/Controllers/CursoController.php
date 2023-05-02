d3b4a64422d96669f56f4b34fd685b219ffb1b0c
Pablo Inzunza <56041130+bolrak@users.noreply.github.com>
2023-05-02 15:45:19 -0400

    clase 20

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -5,25 +5,50 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Curso;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreCurso;
 
 class CursoController extends Controller
 {
     public function index()
     {
-        $cursos = Curso::paginate();
-
+        $cursos = Curso::orderBy('id', 'desc')->paginate();
         return view('cursos.index', compact('cursos'));
     }
 
     public function create()
     {
-
         return view('cursos.create');
     }
 
-    public function show($id)
+    public function store(StoreCurso $request)
     {
-        $curso = Curso::find($id);
+        $curso = new Curso();
+
+        $curso->name = $request->name;
+        $curso->description = $request->description;
+        $curso->categoria = $request->categoria;
+
+        $curso->save();
+        return redirect()->route('cursos.show', $curso);
+    }
+
+    public function show(Curso $curso)
+    {
         return view('cursos.show', compact('curso'));
+    }
+
+    public function edit(Curso $curso)
+    {
+        return view('cursos.edit', compact('curso'));
+    }
+
+    public function update(StoreCurso $request, Curso $curso)
+    {
+        $curso->name = $request->name;
+        $curso->description = $request->description;
+        $curso->categoria = $request->categoria;
+
+        $curso->save();
+        return redirect()->route('cursos.show', $curso);
     }
 }
